61eca1f48858571ac141ac6482c8d8a3920afaee
michaelbolack <michaelbolack@gmail.com>
2026-08-22 14:03:13 -0400

    Export generic district race builder

--- scripts/election_core/__init__.py
+++ scripts/election_core/__init__.py
@@ -1,5 +1,12 @@
 """State-agnostic Election Center ingestion primitives."""
 
 from .districts import DistrictScope, ValidationResult, checksum_geographies, make_race_id
+from .adapter import build_district_race
 
-__all__ = ["DistrictScope", "ValidationResult", "checksum_geographies", "make_race_id"]
+__all__ = [
+    "DistrictScope",
+    "ValidationResult",
+    "checksum_geographies",
+    "make_race_id",
+    "build_district_race",
+]
